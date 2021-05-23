62f03f0769a93bc15813bec85e03e2a66c7d0629
tony onserio <onseriocodes@gmail.com>
2021-05-22 19:35:47 -0500

    Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,28 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
+import { CardBody, CardFooter } from 'shards-react';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
+    <Card className='my-3  rounded' id='card'>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
 
-      <Card.Body>
-        <a href={`/product/${product._id}`}>
+      <CardBody>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
-            <strong>{product.name} </strong>{' '}
+            <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
-        <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews{' '}
-          </div>
+        <Card.Text>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
-
-        <Card.Text as='h3'>${product.price}</Card.Text>
-      </Card.Body>
+      </CardBody>
+      <CardFooter>
+        <h4 className='center'>${product.price}</h4>
+      </CardFooter>
     </Card>
   );
 };
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { FaRegStar, FaStarHalf, FaStar, FaStarHalfAlt } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+      <span>
+        <i style={{ color }}>
+          {value >= 1 ? (
+            <FaStar />
+          ) : value >= 0.5 ? (
+            <FaStarHalf />
+          ) : (
+            <FaRegStar />
+          )}
+        </i>
+
+        <i style={{ color }}>
+          {' '}
+          {value >= 2 ? (
+            <FaStar />
+          ) : value >= 1.5 ? (
+            <FaStarHalf />
+          ) : (
+            <FaRegStar />
+          )}
+        </i>
+
+        <i style={{ color }}>
+          {' '}
+          {value >= 3 ? (
+            <FaStar />
+          ) : value >= 2.5 ? (
+            <FaStarHalf />
+          ) : (
+            <FaRegStar />
+          )}
+        </i>
+
+        <i style={{ color }}>
+          {' '}
+          {value >= 4 ? (
+            <FaStar />
+          ) : value >= 3.5 ? (
+            <FaStarHalf />
+          ) : (
+            <FaRegStar />
+          )}
+        </i>
+
+        <i style={{ color }}>
+          {' '}
+          {value >= 5 ? (
+            <FaStar />
+          ) : value >= 4.5 ? (
+            <FaStarHalf />
+          ) : (
+            <FaRegStar />
+          )}
+        </i>
+      </span>
+
+      <span>{text && text} </span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: 'orange'
+};
+
+Rating.prototype = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string
+};
+
+export default Rating;
