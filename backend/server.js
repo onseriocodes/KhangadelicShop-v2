070f7ffb62bdd9ff34144b93f30ebb16cc358107
tony onserio <onseriocodes@gmail.com>
2021-07-05 11:14:59 -0500

    Connect To DB w/ Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,34 @@
-// SERVER
-const express = require('express');
-const products = require('./data/products');
+// SERVER.JS
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
+// ROUTE: ROOT
 app.get('/', (req, res) => {
-  res.send('API IS RUNNING!');
+  res.send('API IS RUNNING...');
 });
 
+// ROUTE: GET ALL PRODUCTS
 app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
+// ROUTE: GET SINGLE PRODUCT:
 app.get('/api/products/:id', (req, res) => {
   const product = products.find(p => p._id === req.params.id);
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  5000,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
