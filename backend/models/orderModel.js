b708d6ac0e4a6029ac60484edc98e91b6535e8ba
tony onserio <onseriocodes@gmail.com>
2021-07-05 13:26:05 -0500

    Seeder Script data to db

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -32,10 +32,10 @@ const orderSchema = mongoose.Schema(
       required: true
     },
     paymentResult: {
-      id: { type: string },
-      status: { type: string },
-      update_time: { type: string },
-      email_address: { type: string }
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String }
     },
     taxPrice: {
       type: Number,
